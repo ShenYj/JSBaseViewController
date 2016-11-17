65022dd353df062e26b1b65896363595b3b5de2c
ShenYj <shenyanjie123@foxmail.com>
2016-11-17 22:39:16 +0800

    新建工程

---
+++ BaseViewController/BaseViewController/BaseViewController/JSNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  JSNavigationController.h
+//  BaseViewController
+//
+//  Created by ShenYj on 2016/11/17.
+//  Copyright © 2016年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSNavigationController : UINavigationController
+
+@end
---
+++ BaseViewController/BaseViewController/Demos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseViewController
+//
+//  Created by ShenYj on 2016/11/17.
+//  Copyright © 2016年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
