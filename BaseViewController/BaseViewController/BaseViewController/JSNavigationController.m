05ab78d5a34769e6f963ed7e17d6d08ac9378018
ShenYj <shenyanjie123@foxmail.com>
2016-11-18 12:49:59 +0800

    返回按钮标题处理

--- BaseViewController/BaseViewController/BaseViewController/JSNavigationController.m
+++ BaseViewController/BaseViewController/BaseViewController/JSNavigationController.m
@@ -25,19 +25,44 @@
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
     
-    // 设置返回按钮
-    if ([viewController isKindOfClass:[JSBaseViewController class]]) {
-        JSBaseViewController *baseVC = (JSBaseViewController *)viewController;
-        baseVC.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:@"返回" withFont:16 withTarget:self withAction:@selector(goBackToParentController:)];
-    }
-    
-    // 设置全局隐藏底部TabBar
-    if (self.bottomBarHiddenWhenPushed) {
+    if (self.childViewControllers.count > 0) {
         
-        if (self.childViewControllers.count > 0) {
+        // 设置全局隐藏底部TabBar
+        if (self.bottomBarHiddenWhenPushed) {
             viewController.hidesBottomBarWhenPushed = YES;
         }
+        
+        // 设置返回按钮
+        if ([viewController isKindOfClass:[JSBaseViewController class]]) {
+            JSBaseViewController *parentVC = self.childViewControllers[self.childViewControllers.count-1];
+            JSBaseViewController *nextVC = (JSBaseViewController *)viewController;
+            nextVC.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:parentVC.js_navigationItem.title withFont:16 withTarget:self withAction:@selector(goBackToParentController:)];
+            if (self.childViewControllers.count > 1) {
+                nextVC.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:@"返回" withFont:16 withTarget:self withAction:@selector(goBackToParentController:)];
+            }
+            
+        }
+        
+
     }
+    
+//    // 设置返回按钮
+//    if ([viewController isKindOfClass:[JSBaseViewController class]]) {
+//        JSBaseViewController *baseVC = (JSBaseViewController *)viewController;
+//        if (self.childViewControllers.count > 0) {
+//            baseVC.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:@"返回" withFont:16 withTarget:self withAction:@selector(goBackToParentController:)];
+//        } else {
+//            baseVC.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:@"首页" withFont:16 withTarget:self withAction:@selector(goBackToParentController:)];
+//        }
+//    }
+//    
+//    // 设置全局隐藏底部TabBar
+//    if (self.bottomBarHiddenWhenPushed) {
+//        
+//        if (self.childViewControllers.count > 0) {
+//            viewController.hidesBottomBarWhenPushed = YES;
+//        }
+//    }
     
     [super pushViewController:viewController animated:animated];
 }
--- BaseViewController/BaseViewController/Demos/ViewController.m
+++ BaseViewController/BaseViewController/Demos/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) UITextView *textView;
+
 @end
 
 @implementation ViewController
@@ -23,6 +25,16 @@
     self.js_navigationItem.title = @"首页";
     self.js_navigationItem.leftBarButtonItem = [[JSBaseNavBarButtonItem alloc] initWithTitle:@"首页" withFont:16 withTarget:self withAction:@selector(clickLeftBarButtonItem:)];
     
+    [self.view addSubview:self.textView];
+    self.textView.translatesAutoresizingMaskIntoConstraints = NO;
+    NSLayoutConstraint *left = [NSLayoutConstraint constraintWithItem:self.textView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1 constant:0];
+    [self.view addConstraint:left];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.textView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1 constant:64];
+    [self.view addConstraint:top];
+    NSLayoutConstraint *right = [NSLayoutConstraint constraintWithItem:self.textView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1 constant:0];
+    [self.view addConstraint:right];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.textView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
+    [self.view addConstraint:bottom];
     
 }
 
@@ -41,6 +53,22 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark
+#pragma mark - lazy
+- (UITextView *)textView {
+    
+    if (!_textView) {
+        _textView = [[UITextView alloc] init];
+        _textView.font = [UIFont systemFontOfSize:15];
+        _textView.textColor = [UIColor blackColor];
+        _textView.alwaysBounceVertical = YES;
+        _textView.alwaysBounceHorizontal = NO;
+        _textView.textAlignment = NSTextAlignmentLeft;
+        _textView.text = @"这里使用的是自定义的Navigationbar\n1.通过自定义NavigationController隐藏默认navigationbar属性\n2.在BaseViewController中自定义Navigationbar和NavigationItem属性,并添加至view中\n3.重写title的Setter方法,在方法内给自定义的navigationItem的title属性赋值\n4.在ViewController(演示控制器)中设置了左侧自定义buttonItem Push到一个新的DemoVC\n5.在DemoVC中设置了右侧自定义buttonItem Pop回上级控制器\n6.在自定义NavigationController中重写pushViewController:方法,再次获取到将要Push的下级控制器,并设置其左侧按钮Pop回上一级功能\n7.在pushViewController:方法中,同时对底部TabBar隐藏做处理\n";
+    }
+    return _textView;
 }
 
 @end
